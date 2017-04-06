09a532700f955a8145a3c7f9f48cff3a3b04558e
bhuguet <bhuguet@purch.com>
2017-04-06 13:46:40 +0200

    fixed ssh for git clone purpose

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM ubuntu:latest
 ENV BOTDIR /opt/bot
 
 RUN apt-get update && \
-  apt-get install -y wget vim && \
+  apt-get install -y wget vim curl && \
   wget -q -O - https://deb.nodesource.com/setup_6.x | bash - && \
   apt-get install -y git build-essential nodejs && \
   rm -rf /var/lib/apt/lists/*
@@ -17,6 +17,18 @@ RUN apt-get update
 RUN apt-get install -y xvfb x11-xkb-utils xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic redis-server
 
 WORKDIR ${BOTDIR}
+
+# Make ssh dir
+RUN mkdir -p /root/.ssh/
+
+# Copy over private key, and set permissions
+ADD id_rsa /root/.ssh/id_rsa
+
+# Create known_hosts
+RUN touch /root/.ssh/known_hosts && \
+  ssh-keyscan bitbucket.org >> /root/.ssh/known_hosts
+RUN mkdir -p ${BOTDIR}/tools
+RUN git clone --depth=1 git@bitbucket.org:bhuguet/serialjiracreator.git ${BOTDIR}/tools/serialjiracreator
        
 ENV HUBOT_PORT 8080
 ENV PORT ${HUBOT_PORT}
@@ -24,6 +36,7 @@ EXPOSE ${HUBOT_PORT}
 
 RUN npm install
 RUN npm install --save hubot-hipchat
+RUN npm install --save hubot-jira
 
-CMD /bin/sh ${BOTDIR}/hubot-run.sh
-#CMD tail -f /dev/null
+#CMD /bin/sh ${BOTDIR}/hubot-run.sh
+CMD tail -f /dev/null
--- scripts/essais.js
+++ scripts/essais.js
@@ -1,85 +1,20 @@
-# Description:
-#   Example scripts for you to examine and try out.
-#
-# Notes:
-#   They are commented out by default, because most of them are pretty silly and
-#   wouldn't be useful and amusing enough for day to day huboting.
-#   Uncomment the ones you want to try and experiment with.
-#
-#   These are from the scripting documentation: https://github.com/github/hubot/blob/master/docs/scripting.md
-#
-# Commands:
-#   hubot java - Réponds Java bien ! (en javascript)
+// Description:
+//   Example scripts for you to examine and try out.
+//
+// Notes:
+//   They are commented out by default, because most of them are pretty silly and
+//   wouldn't be useful and amusing enough for day to day huboting.
+//   Uncomment the ones you want to try and experiment with.
+//
+//   These are from the scripting documentation: https://github.com/github/hubot/blob/master/docs/scripting.md
+//
+// Commands:
+//   hubot java - Réponds Java bien ! (en javascript)
 
 module.exports = function(robot) {
 
-  robot.hear('/java/i', function(res){
+  robot.hear(/java/i, function(res){
     res.send('Java bien ! (en javascript)');
   });
 
 }
-  
-  # answer = process.env.HUBOT_ANSWER_TO_THE_ULTIMATE_QUESTION_OF_LIFE_THE_UNIVERSE_AND_EVERYTHING
-  #
-  # robot.respond /what is the answer to the ultimate question of life/, (res) ->
-  #   unless answer?
-  #     res.send "Missing HUBOT_ANSWER_TO_THE_ULTIMATE_QUESTION_OF_LIFE_THE_UNIVERSE_AND_EVERYTHING in environment: please set and try again"
-  #     return
-  #   res.send "#{answer}, but what is the question?"
-  #
-  # robot.respond /you are a little slow/, (res) ->
-  #   setTimeout () ->
-  #     res.send "Who you calling 'slow'?"
-  #   , 60 * 1000
-  #
-  # annoyIntervalId = null
-  #
-  # robot.respond /annoy me/, (res) ->
-  #   if annoyIntervalId
-  #     res.send "AAAAAAAAAAAEEEEEEEEEEEEEEEEEEEEEEEEIIIIIIIIHHHHHHHHHH"
-  #     return
-  #
-  #   res.send "Hey, want to hear the most annoying sound in the world?"
-  #   annoyIntervalId = setInterval () ->
-  #     res.send "AAAAAAAAAAAEEEEEEEEEEEEEEEEEEEEEEEEIIIIIIIIHHHHHHHHHH"
-  #   , 1000
-  #
-  # robot.respond /unannoy me/, (res) ->
-  #   if annoyIntervalId
-  #     res.send "GUYS, GUYS, GUYS!"
-  #     clearInterval(annoyIntervalId)
-  #     annoyIntervalId = null
-  #   else
-  #     res.send "Not annoying you right now, am I?"
-  #
-  #
-  # robot.router.post '/hubot/chatsecrets/:room', (req, res) ->
-  #   room   = req.params.room
-  #   data   = JSON.parse req.body.payload
-  #   secret = data.secret
-  #
-  #   robot.messageRoom room, "I have a secret: #{secret}"
-  #
-  #   res.send 'OK'
-  #
-  # robot.error (err, res) ->
-  #   robot.logger.error "DOES NOT COMPUTE"
-  #
-  #   if res?
-  #     res.reply "DOES NOT COMPUTE"
-  #
-  # robot.respond /have a soda/i, (res) ->
-  #   # Get number of sodas had (coerced to a number).
-  #   sodasHad = robot.brain.get('totalSodas') * 1 or 0
-  #
-  #   if sodasHad > 4
-  #     res.reply "I'm too fizzy.."
-  #
-  #   else
-  #     res.reply 'Sure!'
-  #
-  #     robot.brain.set 'totalSodas', sodasHad+1
-  #
-  # robot.respond /sleep it off/i, (res) ->
-  #   robot.brain.set 'totalSodas', 0
-  #   res.reply 'zzzzz'
