b6adf54f18d016e8ae9eb35d64c477b35ee819b0
bhuguet <bhuguet@purch.com>
2017-05-10 15:29:08 +0200

    update hubot-jira and serialjiracreator on startup

--- hubot-run.sh
+++ hubot-run.sh
@@ -1,3 +1,9 @@
+echo -e " \nUpdate hubot-jira and serialjiracreator"
+npm install hubot-jira
+cd tools/serialjiracreator
+git pull --rebase origin master
+cd -
+
 /usr/bin/redis-server &
 sleep 10; bin/hubot --adapter hipchat --name hubot &
 wait
